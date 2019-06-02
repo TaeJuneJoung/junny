75252b8411eb161c5f297af0de01d4fd1bbed123
Dev_J <jtj0525@gmail.com>
2019-06-02 15:26:59 +0000

    deploy

--- junny/settings.py
+++ junny/settings.py
@@ -16,8 +16,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
